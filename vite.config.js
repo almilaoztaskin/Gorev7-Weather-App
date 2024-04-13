7be33d8aad3af05ce08ef83935e71d39be3dcb16
Almıla <almilaoztaskin@gmail.com>
2024-04-13 21:48:51 +0300

    Update base URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Weather-App-Gorev7/",
+  base:"/Gorev7-Weather-App/",
   plugins: [react()],
 })
